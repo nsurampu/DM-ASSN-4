8bbc6dc77f17c616078cf9babd86b6b2aa9d2abe
Naren Surampudi <nsurampu@gmail.com>
2019-04-22 01:29:10 +0530

    Minor changes

--- kNN.py
+++ kNN.py
@@ -5,69 +5,71 @@ import operator
 import pandas as pd
 from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
 
-def loadDataset(filename, split, trainingSet=[] , testSet=[]):
-	dataset = pd.read_csv(filename)
-	dataset = dataset[1:100]
-	for x in range(len(dataset)-1):
-		if random.random() < split:
-			trainingSet.append(dataset.iloc[x])
-		else:
-			testSet.append(dataset.iloc[x])
+class KNN:
+	def loadDataset(self, filename, split, trainingSet=[] , testSet=[]):
+		dataset = pd.read_csv(filename)
+		dataset = dataset[1:100]
+		for x in range(len(dataset)-1):
+			if random.random() < split:
+				trainingSet.append(dataset.iloc[x])
+			else:
+				testSet.append(dataset.iloc[x])
 
-def euclideanDistance(instance1, instance2, length):
-	distance = 0
-	for x in range(length):
-		distance += pow((instance1[x] - instance2[x]), 2)
-	return math.sqrt(distance)
+	def euclideanDistance(self, instance1, instance2, length):
+		distance = 0
+		for x in range(length):
+			distance += pow((instance1[x] - instance2[x]), 2)
+		return math.sqrt(distance)
 
-def getNeighbors(trainingSet, testInstance, k):
-	distances = []
-	length = len(testInstance)-1
-	for x in range(len(trainingSet)):
-		dist = euclideanDistance(testInstance, trainingSet[x], length)
-		distances.append((trainingSet[x], dist))
-	distances.sort(key=operator.itemgetter(1))
-	neighbors = []
-	for x in range(k):
-		neighbors.append(distances[x][0])
-	return neighbors
+	def getNeighbors(self, trainingSet, testInstance, k):
+		distances = []
+		length = len(testInstance)-1
+		for x in range(len(trainingSet)):
+			dist = self.euclideanDistance(testInstance, trainingSet[x], length)
+			distances.append((trainingSet[x], dist))
+		distances.sort(key=operator.itemgetter(1))
+		neighbors = []
+		for x in range(k):
+			neighbors.append(distances[x][0])
+		return neighbors
 
-def getResponse(neighbors):
-	classVotes = {}
-	for x in range(len(neighbors)):
-		response = neighbors[x][-1]
-		if response in classVotes:
-			classVotes[response] += 1
-		else:
-			classVotes[response] = 1
-	# print(classVotes)
-	sortedVotes = sorted(classVotes.items(), key=operator.itemgetter(1), reverse=True)
-	# print(sortedVotes)
-	return sortedVotes[0][0]
+	def getResponse(self, neighbors):
+		classVotes = {}
+		for x in range(len(neighbors)):
+			response = neighbors[x][-1]
+			if response in classVotes:
+				classVotes[response] += 1
+			else:
+				classVotes[response] = 1
+		# print(classVotes)
+		sortedVotes = sorted(classVotes.items(), key=operator.itemgetter(1), reverse=True)
+		# print(sortedVotes)
+		return sortedVotes[0][0]
 
-def getValidationSet(testSet):
-	validations = []
-	for x in range(len(testSet)):
-		validations.append(testSet[x][-1])
-	return validations
+	def getValidationSet(self, testSet):
+		validations = []
+		for x in range(len(testSet)):
+			validations.append(testSet[x][-1])
+		return validations
 
-def main():
-	# prepare data
+if __name__ == "__main__":
+	knn = KNN()
+
 	trainingSet=[]
 	testSet=[]
 	split = 0.80
-	loadDataset('nursery_processed.csv', split, trainingSet, testSet)
+	knn.loadDataset('nursery_processed.csv', split, trainingSet, testSet)
 	print('Train set: ' + repr(len(trainingSet)))
 	print('Test set: ' + repr(len(testSet)))
 	# generate predictions
 	predictions=[]
 	k = 5
 	for x in range(len(testSet)):
-		neighbors = getNeighbors(trainingSet, testSet[x], k)
-		result = getResponse(neighbors)
+		neighbors = knn.getNeighbors(trainingSet, testSet[x], k)
+		result = knn.getResponse(neighbors)
 		predictions.append(result)
 		print('Predcted class:' + repr(result) + ', Actual class:' + repr(testSet[x][-1]))
-	validations = getValidationSet(testSet)
+	validations = knn.getValidationSet(testSet)
 	accuracy = accuracy_score(validations, predictions)
 	cf_matrix = confusion_matrix(validations, predictions)
 	class_report = classification_report(validations, predictions)
@@ -76,5 +78,3 @@ def main():
 	print(cf_matrix)
 	print("classification Report: ")
 	print(class_report)
-
-main()
