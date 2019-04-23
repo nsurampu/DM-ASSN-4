edf153662b4c6d86e5f2ff48b516ae626bfe4278
aditya <adityashrikanth14@gmail.com>
2019-04-23 15:54:36 +0530

    done

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,69 +1,95 @@
 import numpy as np 
 import pandas as pd 
 from pprint import pprint
+from sklearn.metrics import confusion_matrix
 
-data = pd.read_csv('nursery.csv',header=None)
-data = data.values
+np.random.seed(100)
 
-attribute_names = data[0,:]
-features = data[1:,:-1]
-labels = data[1:,-1]
-unique_values = list(set(labels.tolist()))
-unique_values.sort()
-num_classes = len(unique_values)
+'nursery.csv'
+def naive_bayes(path_to_file):
+    
+    """ Description
+    :type path_to_file: str
+    :param path_to_file: the path to the .csv file
 
-unique_values = {key.strip():unique_values.index(key) for key in unique_values}
-print(unique_values)
-train_fraction = 0.8
+    :rtype: tuple: the predicted classes and the actual classs
+    """
+    data = pd.read_csv(path_to_file,header=None)
+    data = data.values
+    attribute_names = data[0,:]
+    data = data[1:,:]
+    np.random.shuffle(data)
+    features = data[1:,:-1]
+    labels = data[1:,-1]
+    unique_values = list(set(labels.tolist()))
+    unique_values.sort()
+    num_classes = len(unique_values)
 
-train_size = int(features.shape[0]*train_fraction)
+    unique_values = {key.strip():unique_values.index(key) for key in unique_values}
+    train_fraction = 0.8
 
-train_features = features[:train_size,:].tolist()
-train_labels = labels[:train_size].tolist()
+    train_size = int(features.shape[0]*train_fraction)
 
-test_features = features[train_size:,:].tolist()
-test_labels = labels[train_size:].tolist()
+    train_features = features[:train_size,:].tolist()
+    train_labels = labels[:train_size].tolist()
 
-
-
-conditional_probability_table = {}
-
-for row_index in range(len(train_features)):
-    for attribute_index in range(len(train_features[row_index])):
-        key = attribute_names[attribute_index] + ' : ' + train_features[row_index][attribute_index]
-        # print(key,train_labels[row_index],unique_values[train_labels[row_index]])
-        if key not in conditional_probability_table:
-            conditional_probability_table[key] = [0 for i in range(num_classes)]
-            index = unique_values[train_labels[row_index]]
-            conditional_probability_table[key][index] = 1
-        else:
-            index = unique_values[train_labels[row_index]]
-            conditional_probability_table[key][index] += 1
-
-for key in conditional_probability_table.keys():
-    conditional_probability_table[key] = [item/train_size for item in conditional_probability_table[key]]
+    test_features = features[train_size:,:].tolist()
+    test_labels = labels[train_size:].tolist()
 
 
 
-accuracy = 0
+    conditional_probability_table = {}
+    prior_distribution = {key:0 for key in unique_values.keys()}
 
-predicted_matrix = [0 for i in range(num_classes)]
-actual_matrix = [0 for i in range(num_classes)]
+    for row_index in range(len(train_features)):
+        prior_distribution[train_labels[row_index]] += 1
+        for attribute_index in range(len(train_features[row_index])):
+            key = attribute_names[attribute_index] + ' : ' + train_features[row_index][attribute_index]
+            # print(key,train_labels[row_index],unique_values[train_labels[row_index]])
+            if key not in conditional_probability_table:
+                conditional_probability_table[key] = [0 for i in range(num_classes)]
+                index = unique_values[train_labels[row_index]]
+                conditional_probability_table[key][index] = 1
+            else:
+                index = unique_values[train_labels[row_index]]
+                conditional_probability_table[key][index] += 1
 
-for row_index in range(len(test_features)):
-    result = [1 for i in range(num_classes)]
-    for attribute_index in range(len(test_features[row_index])):
-        key = attribute_names[attribute_index] + ' : ' + test_features[row_index][attribute_index]
-        result = [a*b for a,b in zip(conditional_probability_table[ key],result)]
-    result = np.argmax(result)
-    actual_index = unique_values[test_labels[row_index]]
-    predicted_matrix[result] += 1
-    actual_matrix[actual_index] += 1
-    if actual_index == result:
-        accuracy += 1
+    for key in conditional_probability_table.keys():
+        conditional_probability_table[key] = [item/train_size for item in conditional_probability_table[key]]
+
+    for key in prior_distribution.keys():
+        prior_distribution[key] /= train_size
 
 
-print(accuracy/len(test_features))
-print(predicted_matrix)
-print(actual_matrix)
-print(len(test_features),train_size)
+    accuracy = 0
+
+    predicted_matrix = [0 for i in range(num_classes)]
+    actual_matrix = [0 for i in range(num_classes)]
+
+
+    pred_labels = []
+    actual_labels = []
+    for row_index in range(len(test_features)):
+        result = [prior_distribution[i] for i in prior_distribution.keys()]
+        for attribute_index in range(len(test_features[row_index])):
+            key = attribute_names[attribute_index] + ' : ' + test_features[row_index][attribute_index]
+            result = [a*b for a,b in zip(conditional_probability_table[ key],result)]
+        result = np.argmax(result)
+        actual_index = unique_values[test_labels[row_index]]
+        predicted_matrix[result] += 1
+        actual_matrix[actual_index] += 1
+        pred_labels.append(result)
+        actual_labels.append(actual_index)
+        if actual_index == result:
+            accuracy += 1
+
+
+    # print(prior_distribution)
+    print(accuracy/len(test_features))
+    print('predicted: matrix: ',predicted_matrix)
+    print('actual matrix: ',actual_matrix)
+    # print(len(test_features),train_size)
+    return confusion_matrix(actual_labels,pred_labels)
+
+if __name__ == "__main__":
+    print('confusion matrix: \n',naive_bayes('nursery.csv'))
